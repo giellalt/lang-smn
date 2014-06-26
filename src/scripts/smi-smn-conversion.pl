6e29bee1120cb2676f1f91b6a50d88a9b32aa4de
Trond Trosterud <trond.trosterud@uit.no>
2014-06-26 07:31:35 +0000

    Now it compiles, but is full of dummy lexica.

--- src/scripts/smi-smn-conversion.pl
+++ src/scripts/smi-smn-conversion.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 #
 # smi-sma-conversion.pl
-# Convert names in the SMI propernoun lexicon to sma.
+# Convert names in the SMI propernoun lexicon to smn.
 # $Id: smi-sma-conversion.pl 91035 2014-03-24 22:56:01Z sjur $
 
 use strict;
@@ -9,7 +9,7 @@ use utf8;
 
 while(<>) {
 	# Continuation lexicon substitutions:
-	s/ C-FI-NEN/nen LONDON/g ;
+#	s/ C-FI-NEN/nen LONDON/g ;
 	s/SUND/BERN/g ;
 	s/HEIM/BERN/g ;
 	s/NIKOSIIJA/ACCRA/g ;
@@ -17,6 +17,14 @@ while(<>) {
 	s/BALAK/ANAR/g ;
 	s/HAWAII/ACCRA/g ;
 	s/SKANIK/SULLOT/g ;
+	s/RONDANE-LOAN/BERN/g ;
+	s/BETFAGE/BERN/g ;
+	s/DUORTNUS/BERN/g ;
+	s/ERVASTI/ACCRA/g ;
+	s/DUBAI/BERN/g ;
+	s/BETFAGE/BERN/g ;
+
+
 
 	s/b9/b/g ;
 	s/d9/d/g ;
@@ -41,45 +49,12 @@ while(<>) {
 #	s/st /st9 /g ;
 
 	# Substitutions due to orthographic differences between SMJ and SME:
-	s/t:(.*)h /t:$1d9 /g ;
-	s/čč/ttj/g ;
-	s/Č/Tj/g ;
+#	s/čč/ttj/g ;
 	# Andreevič -> Andreevitj:
-	s/^(.*)č /$1tj:$1t9j /g ;
-	s/č/tj/g ;
-	s/šž/sjtj/g ;
-	s/Šž/Sjtj/g ;
-	s/šš/ssj/g ;
-	s/žž/dtj/g ;
-	s/Š/Sj/g ;
-	s/š/sj/g ;
-	s/ž/dj/g ;
-	# j->i || Vow i .* : .* Vow _ ; Heaika:Heajka -> Heaika:Heaika. But Majken 	
-	s/([ÁAEIOUaáeiou])i(.*):(.*)([ÁAEIOUaáeiou])j/$1i$2:$3$4i/g ;
+#	s/č/tj/g ;
 
 	my $line = $_;
 
-#	# Special treatment of æ in SMJ:
-#	if ($line =~ /æ/) {
-#			# Replace space in multipart names temporarily with $.
-#		$line =~ s/% /\$/g;
-#		
-#		$line =~ s/^\s+//;
-#		
-#		my ($word, $rest) = split (/\s+/, $line, 2);
-#		$word =~ s/\$/% /g;
-#		if ($line !~ /\:/) {
-#			( my $int_word = $word )     =~ s/æ/æ9/g;
-#			$int_word =~ s/ä/ä9/g;
-#			$line = $word . ":" . $int_word . " " . $rest;
-#		}
-#		else {
-#			my ($upper, $lower) = split(/\:/, $word);
-#			( my $int_word = $lower )     =~ s/æ/æ9/g;
-#			$int_word =~ s/ä/ä9/g;
-#			$line = $upper . ":" . $int_word . " " . $rest;
-#		}
-#	}
 	print $line;
 }
 
