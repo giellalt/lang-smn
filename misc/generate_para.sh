82720e2e964e14e4417ae83e960dbbe5da8e8dcd
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2015-06-26 08:26:38 +0000

    quick-n-dirty script for word form generation

---
+++ misc/generate_para.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command:
+# sh generate_para.sh PATTERN
+
+PATTERN=$1
+L_FILE="in.txt"
+grep $PATTERN ../../src/morphology/stems/nouns.lexc |cut -d ':' -f1>$L_FILE
+
+P_FILE="testnounpradigm.txt"
+
+for lemma in $(cat $L_FILE);
+do
+ for form in $(cat $P_FILE);
+ do
+   echo "${lemma}${form}"
+ done
+done
+
