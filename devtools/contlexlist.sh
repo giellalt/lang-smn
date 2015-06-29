429caa8cf289a4464ce3e9c2f8521fa41acd8673
Lene Antonsen <lene.antonsen@uit.no>
2015-06-29 11:36:50 +0000

    Docu
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/smn@116631 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- devtools/contlexlist.sh
+++ devtools/contlexlist.sh
@@ -1,2 +1,5 @@
+# sh devtools/contlexlist.sh
+# makes a sorted list with all continuation lexicons and their members
+
 grep ';' src/morphology/stems/nouns.lexc | cut -d ' ' -f1,2 |awk '{print $2" "$1}' |rev | sort | rev |see
 
