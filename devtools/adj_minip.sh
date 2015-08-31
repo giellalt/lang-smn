97bd13f1874c1fb9c24e3f2164cf350ce49816cf
Lene Antonsen <lene.antonsen@uit.no>
2015-08-31 06:03:37 +0000

    Remove the old script and check in two variants, for generating miniparadigm of nouns and adjectives. As variable can be used both continuation lexiconname and lemma.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/smn@119756 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ devtools/adj_minip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command:
+# sh generate_contlex_para.sh PATTERN
+# example, when you are in smn:
+# sh devtools/generate_contlex_para.sh LAAVU | dsmnDict | see
+
+PATTERN=$1
+L_FILE="in.txt"
+cut -d '!' -f1 src/morphology/stems/adjectives.lexc | grep $PATTERN | cut -d ':' -f1>$L_FILE
+
+P_FILE="test/data/testadjparadigm.txt"
+
+for lemma in $(cat $L_FILE);
+do
+ for form in $(cat $P_FILE);
+ do
+   echo "${lemma}${form}"
+ done
+done
+
