d1ab152fe2cef246e4efd3410a250208426873f8
Lene Antonsen <lene.antonsen@uit.no>
2015-09-10 13:23:46 +0000

    Exeption for plural lexicons.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/smn@120716 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- devtools/generate-noun-wordforms.sh
+++ devtools/generate-noun-wordforms.sh
@@ -9,13 +9,14 @@ morf_codes="+N+Sg+Nom \
             +N+Sg+Gen \
             +N+Sg+Ill \
             +N+Sg+Loc \
-            +N+Sg+Com"
+            +N+Sg+Com \
+            +N+Pl+Nom"
 
 # Lexicon source file for lexicons and lemmas:
 source_file=src/morphology/stems/nouns.lexc
 
 # Lexicons that should NOT be used to extract lemmas (egrep expression):
-exception_lexicons="(nounstems|KOLME|NELJA)"
+exception_lexicons="(nounstems|KOLME|NELJA|Rnoun|2PUUVSAH|2SAAGIH|2SAANIH|2SKERREEH|3JESANEH|3PAPAREH|3SOKSAMEH|3STEVILEH|3TONALEH|3VALJIIMEH|C_KILDEEH|3VANHIMEH)"
 
 # FST used for generation, MINUS suffix:
 generator_file=src/generator-gt-norm
