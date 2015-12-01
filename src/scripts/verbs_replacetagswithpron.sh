8ddf40019e897d49a5d609e8c74caeea4772c919
Lene Antonsen <lene.antonsen@uit.no>
2015-12-01 10:31:24 +0000

    Script for makin human readable paradigms for verbs.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/smn@126055 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ src/scripts/verbs_replacetagswithpron.sh
@@ -0,0 +1,8 @@
+#sh devtools/verb_minip.sh '(iä|uá).(|.|..)uđ:' > genudverbs.txt
+# cat genudverbs.txt |sh src/scripts/replacetagswithpron.sh | cut -d ' ' -f2- | grep -v '^$' | tr '¢' '\n' > output.txt 
+
+
+
+sed 's/+V+Inf/ ¢infinitiivi:/' |sed 's/+V+Ind+Prs+Sg1/ Onne mun/' | sed 's/+V+Ind+Prs+Sg3/ Onne sun/'| sed 's/+V+Ind+Prs+Du1/ Onne muoi/'| sed 's/+V+Ind+Prs+Pl3/ Onne sij/'| sed 's/+V+Ind+Prt+Sg1/ Jieht mun/'| sed 's/+V+Ind+Prt+Sg3/ Jieht sun/'| sed 's/+V+Ind+Prt+Pl3/ Jieht sij/'| sed 's/+V+Ind+Prs+ConNeg/ Onne ij/'
+
+
