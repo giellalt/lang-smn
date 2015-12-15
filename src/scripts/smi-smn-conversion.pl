13bf155452ac77f6c94a6d460cd3ab20beb2bad0
leneantonsen <lene.antonsen@uit.no>
2015-12-15 13:22:21 +0000

    Removed lines, we don't want to change the lemma itself.

--- src/scripts/smi-smn-conversion.pl
+++ src/scripts/smi-smn-conversion.pl
@@ -25,13 +25,6 @@ while(<>) {
 	s/fkagK/K/g ;
 	s/Maarit LONDON/Maarit MAARIT/g ;
 
-# loanwords with compound border over identical vowels
-
-	s/Hjarteelva/Hjarte-elva/g ;
-	s/Indreeide/Indre-eide/g ;
-	s/Lilleeng/Lille-eng/g ;
-	s/Simleelva/Simle-elva/g ;
-
 
 # sme special symbols
 	s/\^//g ;
