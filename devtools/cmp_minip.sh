dbf8c2591da8974d31f2b1b341c3fcd74dc98272
Lene Antonsen <lene.antonsen@uit.no>
2016-01-10 13:23:21 +0000

    script for testing compounding. The test is with visti as second part. It would be good to test a couple of lemmas from each contlex like this: sh devtools/cmp_minip.sh '^nieidâ:'
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/smn@127897 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ devtools/cmp_minip.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command:
+# sh generate_contlex_para.sh PATTERN
+# example, when you are in smn:
+# sh devtools/noun_minip.sh LAAVU | less
+# Only get the lemma you ask for:
+# sh devtools/cmp_minip.sh '^nieidâ:' 
+
+
+LOOKUP=$(echo $LOOKUP)
+GTHOME=$(echo $GTHOME)
+
+
+PATTERN=$1
+L_FILE="in.txt"
+cut -d '!' -f1 src/morphology/stems/nouns.lexc | egrep $PATTERN | cut -d ':' -f1>$L_FILE
+
+P_FILE="test/data/cmp_paradigm.txt"
+
+for lemma in $(cat $L_FILE);
+do
+ for form in $(cat $P_FILE);
+ do
+   echo "${lemma}${form}" | $LOOKUP $GTHOME/langs/smn/src/generator-gt-norm.xfst
+ done
+done
+
