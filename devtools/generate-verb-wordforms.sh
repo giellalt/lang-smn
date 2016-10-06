30ba2e6ef12aff1c102bbbe42688fe57eaf9f55b
Sjur Moshagen <sjurnm@mac.com>
2016-10-06 15:34:41 +0000

    [Template merge - langs/und] Three template merges in one:
    1) Changed variable name in devtool scripts, to reflect similar changes elsewhere. Part of fixing bug #2219.
    2) Changed variable name and definition to allow overriding the path to the called script, to make it easy to use a locally modified script instead.
    3) More robustness added to the test scripts: checking several variables, testing whether the found variables are pointing to existing directories, and giving an error message if no directory is found.
    
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/smn@141064 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- devtools/generate-verb-wordforms.sh
+++ devtools/generate-verb-wordforms.sh
@@ -3,6 +3,18 @@
 # A short shell script to test word form generation for all continuation
 # lexicons except the ones listed in the exception list. 
 
+# Path to $GIELLA_CORE - we don't use Autotools for these scripts:
+if test "x$GIELLA_CORE" != "x" -a -d "$GIELLA_CORE" ; then
+    giella_core=$GIELLA_CORE
+elif test "x$GTCORE" != "x" -a -d "$GTCORE" ; then
+    giella_core=$GTCORE
+elif test "x$GTHOME" != "x" -a -d "$GTHOME/giella-core" ; then
+    giella_core=$GTHOME/giella-core
+else
+    echo "ERROR: Neither of $$GIELLA_CORE, $$GTCORE or $$GTHOME defined."
+    exit 1
+fi
+
 ######### USER Variables - change these to your liking: #########
 # Codes for the word forms to be generated - list as many or few as needed:
 morf_codes="+V+Inf \
@@ -33,13 +45,12 @@ generator_file=src/generator-gt-norm
 # How many lemmas maximally for each lexicon:
 lemmacount=6
 
-# Specify path to $GTCORE - we don't use Autotools for these scripts:
-gtcore=$GTCORE
-
+# Specify path to the dir containing the script used for generation:
+script_dir=devtools
 
 ################## DO NOT CHANGE BELOW HERE!!! ##################
-source devtools/generate-wordforms-for-cont_lexes.sh \
-        "$gtcore" \
+source $script_dir/generate-wordforms-for-cont_lexes.sh \
+        "$giella_core" \
         "$morf_codes" \
         "$source_file" \
         "$generator_file" \
