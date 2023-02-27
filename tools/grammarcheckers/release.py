4901105078062c3530d681a927a6e5926bde1cab
Trond Trosterud <trond.trosterud@uit.no>
2023-02-27 16:09:26 +0100

    Intermediate python script for release grammarchecker
    This file makes grammarchecker-release.grc from grammarchecker.grc. This presupposes that rules *not* scheduled for release are marked with "ADD:x" whereas releasing a rule means removing the x (default is thus that the rule is not released).
    
    TODO further down: Integrate this in the makefile setup so that a file grammarchecker-release.cg3 is built automatically. In order to make this genral, the sme files should be renamed as follows:
    - grammachecker.cg3 > grammarchecker-archive.cg3
    - grammarchecker-release.cg3 > grammarchecker.cg3
    - ... and the make setup will then treat grammarchecker-release.cg3 as a derived file (perhaps even as grammarchecker-release.bin) for the purpose of the scripts in the modes catalogue.

---
+++ tools/grammarcheckers/release.py
@@ -0,0 +1,16 @@
+
+from sys import stdin
+
+commenting = False
+for line in stdin:
+  line = line.strip()
+
+  if line.startswith("ADD:x"):
+    commenting = True
+  elif line.startswith("COPY"):
+    commenting = False
+
+  if commenting:
+    print("# " + line)
+  else:
+    print(line)
