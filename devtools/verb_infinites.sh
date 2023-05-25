5f7f58ff7e5d32291db8daabcf089d8d17d279a2
Trond Trosterud <trond.trosterud@uit.no>
2023-05-25 21:55:33 +0100

    hfst

--- devtools/verb_infinites.sh
+++ devtools/verb_infinites.sh
@@ -7,6 +7,7 @@
 
 
 LOOKUP=$(echo $LOOKUP)
+HLOOKUP=$(echo $HLOOKUP)
 GTHOME=$(echo $GTHOME)
 
 
@@ -20,7 +21,7 @@ for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
-   echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-smn/src/generator-gt-norm.xfst # xfst
+   echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-smn/src/generator-gt-norm.hfstol # xfst
 #      echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-smn/src/generator-gt-norm.hfstol # hfst
  done
 done
