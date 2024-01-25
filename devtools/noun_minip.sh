c7c6f2618e91e534e108ccecfd44a571dd373d00
leneantonsen <lene.antonsen@uit.no>
2024-01-25 17:47:15 +0100

    fiksa stier

--- devtools/noun_minip.sh
+++ devtools/noun_minip.sh
@@ -17,7 +17,7 @@ GTLANGS=$(echo $GTLANGS)
 
 PATTERN=$1
 L_FILE="in.txt"
-cut -d '!' -f1 src/fst/stems/nouns.lexc | egrep $PATTERN | tr '+' ':'| cut -d ':' -f1>$L_FILE
+cut -d '!' -f1 src/fst/morphology/stems/nouns.lexc | egrep $PATTERN |grep -v Err | tr '+' ':'| cut -d ':' -f1>$L_FILE
 
 P_FILE="test/data/testnounpradigm.txt"
 
@@ -25,8 +25,8 @@ for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
-      echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-smn/src/generator-gt-norm.hfstol # hfst
-#      echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-smn/src/generator-gt-norm.xfst # xfst
+      echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-smn/src/fst/generator-gt-norm.hfstol # hfst
+#      echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-smn/src/fst/generator-gt-norm.xfst # xfst
  done
  rm -f $L_FILE
 done
