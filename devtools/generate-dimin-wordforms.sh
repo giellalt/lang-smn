d98dd48f3049ad878f664823df9e1346f00d5444
Trond Trosterud <trond.trosterud@uit.no>
2024-01-26 15:24:45 +0200

    Problem solved, thanks to Flammie

--- devtools/generate-dimin-wordforms.sh
+++ devtools/generate-dimin-wordforms.sh
@@ -19,20 +19,20 @@ fi
 
 ######### USER Variables - change these to your liking: #########
 # Codes for the word forms to be generated - list as many or few as needed:
-morf_codes="+N+Der1+Der/Dimin+N+Sg+Nom \
-            +N+Der1+Der/Dimin+N+Sg+Gen \
-            +N+Der1+Der/Dimin+N+Sg+Ill \
-            +N+Der1+Der/Dimin+N+Sg+Loc \
-            +N+Der1+Der/Dimin+N+Sg+Com \
-            +N+Der1+Der/Dimin+N+Sg+Abe \
-            +N+Der1+Der/Dimin+N+Par \
-            +N+Der1+Der/Dimin+N+Ess \
-            +N+Der1+Der/Dimin+N+Pl+Nom\
-            +N+Der1+Der/Dimin+N+Pl+Gen \
-            +N+Der1+Der/Dimin+N+Pl+Acc \
-            +N+Der1+Der/Dimin+N+Pl+Ill \
-            +N+Der1+Der/Dimin+N+Pl+Loc \
-            +N+Der1+Der/Dimin+N+Pl+Abe"
+morf_codes="+N+Der/Dimin+N+Sg+Nom \
+            +N+Der/Dimin+N+Sg+Gen \
+            +N+Der/Dimin+N+Sg+Ill \
+            +N+Der/Dimin+N+Sg+Loc \
+            +N+Der/Dimin+N+Sg+Com \
+            +N+Der/Dimin+N+Sg+Abe \
+            +N+Der/Dimin+N+Par \
+            +N+Der/Dimin+N+Ess \
+            +N+Der/Dimin+N+Pl+Nom\
+            +N+Der/Dimin+N+Pl+Gen \
+            +N+Der/Dimin+N+Pl+Acc \
+            +N+Der/Dimin+N+Pl+Ill \
+            +N+Der/Dimin+N+Pl+Loc \
+            +N+Der/Dimin+N+Pl+Abe"
 
 # Lexicon source file for lexicons and lemmas:
 source_file=src/fst/morphology/stems/nouns.lexc
