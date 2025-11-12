75b0f688ba5476d70b2bf0d7a3205df1c4db8ba2
Sjur N Moshagen <sjurnm@mac.com>
2025-11-12 15:58:30 +0200

    Whitespace

--- tools/tts/pipeline.ts
+++ tools/tts/pipeline.ts
@@ -6,12 +6,12 @@ import * as speech from './.divvun-rt/speech.ts';
 
 export default function smnTextTTS(entry: StringEntry): Command {
   let x = hfst.tokenize("tokenise", entry, { model_path: "tokeniser-tts-cggt-desc.pmhfst" });
-  x = divvun.blanktag("whitespace", x, { model_path: "analyser-gt-whitespace.hfst" });
-  x = cg3.vislcg3("mwe-dis",        x, { model_path: "mwe-dis.bin" });
-  x = cg3.mwesplit("mwesplit",      x);
-  x = cg3.vislcg3("disamb",         x, { model_path: "disambiguator.bin" });
-  x = cg3.vislcg3("functions",      x, { model_path: "functions.bin" });
-  x = cg3.vislcg3("deps",           x, { model_path: "dependency.bin" });
+  x = divvun.blanktag("whitespace",     x, { model_path: "analyser-gt-whitespace.hfst" });
+  x = cg3.vislcg3("mwe-dis",            x, { model_path: "mwe-dis.bin" });
+  x = cg3.mwesplit("mwesplit",          x);
+  x = cg3.vislcg3("disamb",             x, { model_path: "disambiguator.bin" });
+  x = cg3.vislcg3("functions",          x, { model_path: "functions.bin" });
+  x = cg3.vislcg3("deps",               x, { model_path: "dependency.bin" });
   x = speech.normalize(
     "normaliser", x,
     {
@@ -40,12 +40,12 @@ export default function smnTextTTS(entry: StringEntry): Command {
  */
 export function localTTSTest_dev(entry: StringEntry): Command {
   let x = hfst.tokenize("tokenise", entry, { model_path: "@./tokeniser-tts-cggt-desc.pmhfst" });
-  x = divvun.blanktag("whitespace", x,     { model_path: "@./analyser-gt-whitespace.hfst" });
-  x = cg3.vislcg3("mwe-dis",        x,     { model_path: "@../tokenisers/mwe-dis.cg3" });
-  x = cg3.mwesplit("mwesplit",      x);
-  x = cg3.vislcg3("disamb",         x,     { model_path: "@../../src/cg3/disambiguator.cg3" });
-  x = cg3.vislcg3("functions",      x,     { model_path: "@../../src/cg3/functions.cg3" });
-  x = cg3.vislcg3("deps",           x,     { model_path: "@../../src/cg3/dependency.cg3" });
+  x = divvun.blanktag("whitespace",     x, { model_path: "@./analyser-gt-whitespace.hfst" });
+  x = cg3.vislcg3("mwe-dis",            x, { model_path: "@../tokenisers/mwe-dis.cg3" });
+  x = cg3.mwesplit("mwesplit",          x);
+  x = cg3.vislcg3("disamb",             x, { model_path: "@../../src/cg3/disambiguator.cg3" });
+  x = cg3.vislcg3("functions",          x, { model_path: "@../../src/cg3/functions.cg3" });
+  x = cg3.vislcg3("deps",               x, { model_path: "@../../src/cg3/dependency.cg3" });
   x = speech.normalize(
     "normaliser", x,
     {
